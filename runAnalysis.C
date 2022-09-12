e54026c9917cdbf743e3a2f498770de374cdc9b5
caiolagana <56349647+caiolagana@users.noreply.github.com>
2022-09-12 08:30:28 -0300

    Update runAnalysis.C
    Add local file

--- runAnalysis.C
+++ runAnalysis.C
@@ -49,7 +49,7 @@ void runAnalysis()
         // if you want to run locally, we need to define some input
         TChain* chain = new TChain("aodTree");
         // add a few files to the chain (change this so that your local files are added)
-        chain->Add("AliAOD.root");
+        chain->Add("/sampa/caiolagana/data/AliAOD252.18r.pass3.000296691.root");
         // start the analysis locally, reading the events from the tchain
         mgr->StartAnalysis("local", chain);
     } else {
